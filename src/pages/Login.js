6710d506d30eca6f5fff48be817fe2a5e46e63be
murat <muratakca.mt@gmail.com>
2020-12-10 16:08:08 +0100

    Project updated

---
+++ src/pages/Login.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Dimensions,
+  View,
+  Text,
+  Image,
+  ScrollView,
+  KeyboardAvoidingView,
+} from 'react-native';
+
+import {authStyle} from './styles';
+import {Input, Button} from '../components';
+import auth from '@react-native-firebase/auth';
+
+const Login = (props) => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <KeyboardAvoidingView style={{flex: 1, backgroundColor: '#cfd8dc'}}>
+        <ScrollView contentContainerStyle={{flex: 1}}>
+          <View style={authStyle.container}>
+            <Image
+              style={authStyle.logo}
+              source={require("../assets/logo.jpeg")}
+            />
+            <Text style={authStyle.logoText}>CLARUSCHAT</Text>
+
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
+
+export {Login};
